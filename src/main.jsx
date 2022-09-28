be0424c542175f531d6755edbe0521a647887e35
DrHeinz11 <franco.martin.1998m@gmail.com>
2022-09-28 17:32:11 -0300

    [build] Add new dependencies

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ChakraProvider } from '@chakra-ui/react';
 import App from './App';
-import './index.css';
+import { theme } from './theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-	<React.StrictMode>
+	<ChakraProvider theme={theme}>
 		<App />
-	</React.StrictMode>
+	</ChakraProvider>
 );
---
+++ src/theme.js
@@ -0,0 +1,9 @@
+import { extendTheme } from '@chakra-ui/react';
+
+const colors = {
+	main: {
+		lightGray: '#f2f2f2',
+	},
+};
+
+export const theme = extendTheme({ colors });
